f6acf956d1339957abf6e56ecc0a2fb7a8e05f95
Brent Guistwite <BrentGuistwite@gmail.com>
2018-02-01 12:18:47 -0500

    added 3rd test to reducer

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -22,5 +22,20 @@ describe('Reducer', () => {
     const action = makeGuess(guess);
     const newState = reducer(state, action);
     expect(newState.guesses[newState.guesses.length - 1]).toEqual(guess);
+    const secondGuess = 99;
+    const secondAction = makeGuess(secondGuess);
+    const secondState = reducer(newState, secondAction);
+    expect(secondState.guesses[secondState.guesses.length - 2]).toEqual(guess);
+    expect(secondState.guesses[secondState.guesses.length - 1]).toEqual(secondGuess);
+  });
+  it('should set an initial state', () => {
+    const state = undefined;
+    const action = {
+      type: 'NOT_A_REAL_ACTION>>>',
+    };
+    const newState = reducer(state, action);
+    expect(newState.guesses).toEqual([]);
+    expect(newState.auralStatus).toEqual('');
+    expect(newState.feedback).toEqual('Make your guess!');
   });
 });
